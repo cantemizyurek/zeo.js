d54b827d20cd08333330ece41a7e5c092c9d071c
Can Temizyurek <can@temizyurek.com>
2022-04-21 00:08:35 -0400

    add loopableArray

--- mechanics.js
+++ mechanics.js
@@ -30,22 +30,38 @@ function once(callback) {
 
 //This is a function that you can only run your callback function multiple times unitl given amount hits 0 and you can initilize num to reload in x amount ms
 function reloadable(callback, amount, delay) {
-    let num = amount;
-    
-    function run(){
-        if(num <= 0){
-            if(delay || delay === 0){
-                setTimeout(() =>{
-                    num = amount;
-                }, delay)
-            }else{
-                num = amount;
-            }
-        }else{
-            callback(num);
-        }
-        num--;
-    }
+  let num = amount;
 
-    return run;
+  function run() {
+    if (num <= 0) {
+      if (delay || delay === 0) {
+        setTimeout(() => {
+          num = amount;
+        }, delay);
+      } else {
+        num = amount;
+      }
+    } else {
+      callback(num);
+    }
+    num--;
+  }
+
+  return run;
+}
+
+//This is a helper function that returns an array value and next time it called it will return next array value in the passed array until it reaches the end of the array then it will return to the beginning of the array
+function loopableArray(arr) {
+  let i = 0;
+
+  function run() {
+    if (i >= arr.length - 1) {
+      const val = arr[i];
+      i = 0;
+      return val;
+    }
+    return arr[i++];
+  }
+
+  return run;
 }
